fcd51209fde0c5178865245d24f5acdeb3656b29
Chris Bacon <chris@dunelm.org.uk>
2011-12-08 22:43:28 +0000

    Altering method call resolution

---
+++ Cil2Js/Analysis/VisitorFindCalls.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Mono.Cecil;
+using Cil2Js.Ast;
+
+namespace Cil2Js.Analysis {
+    public class VisitorFindCalls:AstVisitor {
+
+        public static IEnumerable<MethodReference> V(ICode ast) {
+            var v = new VisitorFindCalls();
+            v.Visit(ast);
+            return v.calls;
+        }
+
+        private VisitorFindCalls() { }
+
+        private List<MethodReference> calls = new List<MethodReference>();
+
+        protected override ICode VisitCall(ExprCall e) {
+            this.calls.Add(e.Calling);
+            return base.VisitCall(e);
+        }
+
+        protected override ICode VisitCall(StmtCall s) {
+            this.calls.Add(s.Calling);
+            return base.VisitCall(s);
+        }
+
+    }
+}
---
+++ Cil2Js/Output/IMethodResolver.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Cil2Js.Ast;
+
+namespace Cil2Js.Output {
+    public interface IMethodResolver {
+
+        Expr Transform(ICall call);
+        string GetName(ICall call);
+
+    }
+}
