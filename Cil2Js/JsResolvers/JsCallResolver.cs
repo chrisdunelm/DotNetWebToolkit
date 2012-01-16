f7897a7a38dc13eeca4e855dab7157bf0ded00e1
Chris Bacon <chris@dunelm.org.uk>
2012-01-16 15:09:14 +0000

    Full methods can have custom JS implementations, as well as calls. Split JsResolver class into three files

--- Cil2Js/JsResolvers/JsCallResolver.cs
+++ Cil2Js/JsResolvers/JsCallResolver.cs
@@ -8,82 +8,9 @@ using Cil2Js.Ast;
 using Cil2Js.Utils;
 
 namespace Cil2Js.JsResolvers {
-    public static class JsCallResolver {
+    public static partial class JsResolver {
 
-        private const string TVoid = "System.Void";
-        private const string TObject = "System.Object";
-        private const string TIntPtr = "System.IntPtr";
-        private const string TBoolean = "System.Boolean";
-        private const string TString = "System.String";
-        private const string TChar = "System.Char";
-        private const string TInt32 = "System.Int32";
-        private const string TArray = "System.Array";
-
-        private static string ArrayOf(string type) {
-            return type + "[]";
-        }
-
-        class M {
-
-            class NullIsWildcardEqualityComparer : IEqualityComparer<string> {
-
-                public static readonly IEqualityComparer<string> Instance = new NullIsWildcardEqualityComparer();
-
-                public bool Equals(string x, string y) {
-                    return x == null || y == null || x == y;
-                }
-
-                public int GetHashCode(string obj) {
-                    return 0;
-                }
-            }
-
-            class ValueEqualityComparer : IEqualityComparer<M> {
-                public bool Equals(M x, M y) {
-                    return
-                        (x.ReturnType == null || y.ReturnType == null || x.ReturnType == y.ReturnType) &&
-                        x.FullName == y.FullName &&
-                        Enumerable.SequenceEqual(x.GenericArgTypes, y.GenericArgTypes, NullIsWildcardEqualityComparer.Instance) &&
-                        Enumerable.SequenceEqual(x.ArgTypes, y.ArgTypes, NullIsWildcardEqualityComparer.Instance);
-                }
-
-                public int GetHashCode(M o) {
-                    return o.FullName.GetHashCode();
-                }
-            }
-
-            public static readonly IEqualityComparer<M> ValueEqComparer = new ValueEqualityComparer();
-
-            public static M Def(string returnType, string fullName, params string[] argTypes) {
-                return new M(returnType, fullName, Enumerable.Empty<string>(), argTypes);
-            }
-
-            public static M Def(string returnType, string fullName, string[] genericArgTypes, params string[] argTypes) {
-                return new M(returnType, fullName, genericArgTypes, argTypes);
-            }
-
-            private M(string returnType, string fullName, IEnumerable<string> genericArgTypes, IEnumerable<string> argTypes) {
-                this.ReturnType = returnType;
-                this.FullName = fullName;
-                this.GenericArgTypes = genericArgTypes;
-                this.ArgTypes = argTypes;
-            }
-
-            public M(MethodReference method) {
-                this.ReturnType = method.ReturnType.FullName;
-                this.FullName = method.DeclaringType.FullName + "." + method.Name;
-                this.GenericArgTypes = method.GenericParameters.Select(x => x.FullName).ToArray();
-                this.ArgTypes = method.Parameters.Select(x => x.ParameterType.FullName).ToArray();
-            }
-
-            public string ReturnType { get; private set; }
-            public string FullName { get; private set; }
-            public IEnumerable<string> GenericArgTypes { get; private set; }
-            public IEnumerable<string> ArgTypes { get; private set; }
-
-        }
-
-        private static Dictionary<M, Func<ICall, Expr>> map = new Dictionary<M, Func<ICall, Expr>>(M.ValueEqComparer) {
+        private static Dictionary<M, Func<ICall, Expr>> callMap = new Dictionary<M, Func<ICall, Expr>>(M.ValueEqComparer) {
             { M.Def(TVoid, "System.Action..ctor", TObject, TIntPtr), SystemResolver.ActionFunc_ctor },
             { M.Def(TVoid, "System.Action`1..ctor", TObject, TIntPtr), SystemResolver.ActionFunc_ctor },
             { M.Def(TVoid, "System.Action`2..ctor", TObject, TIntPtr), SystemResolver.ActionFunc_ctor },
@@ -138,6 +65,7 @@ namespace Cil2Js.JsResolvers {
             { M.Def(TBoolean, "System.Object.Equals", TObject), SystemResolver.ObjectEquals },
 
             { M.Def(TVoid, "System.Array.Copy", TArray, TInt32, TArray, TInt32,TInt32, TBoolean), ArrayResolver.Copy },
+            //{ M.Def(TVoid, "System.Array.Clear", TArray, TInt32, TInt32), ArrayResolver.Clear },
 
             { M.Def(TString, "System.Environment.GetResourceFromDefault", TString), EnvironmentResolver.GetResourceFromDefault },
 
@@ -145,12 +73,12 @@ namespace Cil2Js.JsResolvers {
             { M.Def(TString, "System.ThrowHelper.GetArgumentName", "System.ExceptionArgument"), ThrowHelperResolver.GetArgumentName },
         };
 
-        public static Expr Resolve(ICall call) {
+        public static Expr ResolveCall(ICall call) {
             var mRef = call.CallMethod;
             var mDef = mRef.Resolve();
             // A call that needs translating into a javascript call
-            var m = new M(mDef);
-            var fn = map.ValueOrDefault(m);
+            var m = new M(mRef);
+            var fn = callMap.ValueOrDefault(m);
             if (fn != null) {
                 var resolved = fn(call);
                 return resolved;
@@ -178,10 +106,6 @@ namespace Cil2Js.JsResolvers {
             }
             // No special resolution available
             return null;
-        }
-
-        private static string JsCase(string s) {
-            return char.ToLowerInvariant(s[0]) + s.Substring(1);
         }
 
     }
---
+++ Cil2Js/JsResolvers/JsMethodResolver.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Cil2Js.Ast;
+using Mono.Cecil;
+using Cil2Js.Utils;
+
+namespace Cil2Js.JsResolvers {
+    public static partial class JsResolver {
+
+        private static Dictionary<M, Func<Ctx, Stmt>> methodMap = new Dictionary<M, Func<Ctx, Stmt>>(M.ValueEqComparer) {
+            { M.Def(TVoid, "System.Array.Clear", TArray, TInt32, TInt32), ArrayResolver.Clear },
+        };
+
+        public static Stmt ResolveMethod(Ctx ctx) {
+            var m = new M(ctx.MRef);
+            var fn = methodMap.ValueOrDefault(m);
+            if (fn != null) {
+                var resolved = fn(ctx);
+                return resolved;
+            }
+            return null;
+        }
+
+    }
+}
