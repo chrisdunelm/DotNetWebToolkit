ce128a0a090bf832bae3c2692293251697c94b27
Chris Bacon <chris@dunelm.org.uk>
2012-01-23 11:11:25 +0000

    Improved method/type mapping changes started

---
+++ Cil2Js/JsResolvers/Classes/StringBuilder.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DotNetWebToolkit.Cil2Js.JsResolvers.Classes {
+    sealed class StringBuilder {
+
+        public StringBuilder() {
+            this.s = "";
+        }
+
+        public StringBuilder(int capacity) {
+            this.s = "";
+        }
+
+        public StringBuilder(string value) {
+            this.s = value ?? "";
+        }
+
+        public StringBuilder(int capacity, int maxCapacity) {
+            this.s = "";
+        }
+
+        public StringBuilder(string value, int capacity) {
+            this.s = value ?? "";
+        }
+
+        public StringBuilder(string value, int startIndex, int length, int capacity) {
+            this.s = (value ?? "").Substring(startIndex, length);
+        }
+
+        private string s;
+
+        public StringBuilder Append(bool value) {
+            this.s += value.ToString();
+            return this;
+        }
+
+        public StringBuilder Append(byte value) {
+            this.s += value.ToString();
+            return this;
+        }
+
+        public StringBuilder Append(char value) {
+            this.s += value.ToString();
+            return this;
+        }
+
+        public StringBuilder Append(string value) {
+            this.s += value;
+            return this;
+        }
+
+    }
+}
---
+++ Cil2Js/JsResolvers/JsTypeResolver.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using DotNetWebToolkit.Cil2Js.Utils;
+using Mono.Cecil;
+
+namespace DotNetWebToolkit.Cil2Js.JsResolvers {
+    public static partial class JsResolver {
+
+        private static Dictionary<TypeReference, TypeReference> typeMap = new Dictionary<TypeReference, TypeReference>(TypeExtensions.TypeRefEqComparerInstance) {
+
+        };
+
+        public static TypeReference ResolveType(TypeReference type) {
+            var mapped = typeMap.ValueOrDefault(type);
+            return mapped;
+        }
+
+    }
+}
