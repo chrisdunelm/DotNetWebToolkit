23f00d3dea28b9c2422a2b7c582a885b5011bd6e
Chris Bacon <chris@dunelm.org.uk>
2012-01-23 16:47:19 +0000

    Added testing of .ToString() method

---
+++ Test/ExecutionTests/TestToString.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+
+namespace Test.ExecutionTests {
+
+    [TestFixture]
+    public class TestToString : ExecutionTestBase {
+
+        [Test]
+        public void TestString() {
+            Func<string, string> f = a => a.ToString();
+            this.Test(f);
+        }
+
+        [Test, Ignore("Boolean ToString() not yet working")]
+        public void TestBoolean() {
+            Func<string> f = () => true.ToString() + false.ToString();
+            this.Test(f);
+        }
+
+    }
+
+}
