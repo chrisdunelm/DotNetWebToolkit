7b21fc96315b4f762487d35d3878822d8ed0d53f
Chris Bacon <chris@dunelm.org.uk>
2012-02-01 23:09:27 +0000

    Added first test - not yet working

---
+++ Test/ExecutionTests/TestDictionary.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+
+namespace Test.ExecutionTests {
+
+    [TestFixture]
+    public class TestDictionary : ExecutionTestBase {
+
+        [Test, Ignore("Dictionary not yet working")]
+        public void Test1() {
+            Func<int, string, int> f = (i, s) => {
+                var d = new Dictionary<string, int>();
+                d.Add(s, i);
+                return d[s];
+            };
+            this.Test(f);
+        }
+
+    }
+
+}
