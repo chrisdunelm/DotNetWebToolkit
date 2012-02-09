fdc8b84dd04825dd027e95809c5adf6c3f1d9288
Chris Bacon <chris@dunelm.org.uk>
2012-02-09 14:50:21 +0000

    Added Math.Tan()

--- Cil2Js/JsResolvers/Classes/_Math.cs
+++ Cil2Js/JsResolvers/Classes/_Math.cs
@@ -6,11 +6,15 @@ using System.Threading.Tasks;
 
 namespace DotNetWebToolkit.Cil2Js.JsResolvers.Classes {
     class _Math {
+        
+        // TODO: Implement mechanism to allow this to be replaced at call site.
+        // Although the (currently hypothetical) global optimiser should inline these
 
         [Js("return Math.sqrt(a);")]
-        public static double Sqrt(double value) {
-            throw new Exception();
-        }
+        public static double Sqrt(double a) { throw new Exception(); }
+
+        [Js("return Math.tan(a);")]
+        public static double Tan(double a) { throw new Exception(); }
 
     }
 }
