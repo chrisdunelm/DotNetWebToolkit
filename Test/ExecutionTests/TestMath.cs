c7db06911e3ca53ed61047e3f8ba3997d6c340a8
Chris Bacon <chris@dunelm.org.uk>
2012-02-09 14:53:43 +0000

    Added test for Math class

---
+++ Test/ExecutionTests/TestMath.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+
+namespace Test.ExecutionTests {
+
+    [TestFixture]
+    public class TestMath : ExecutionTestBase {
+
+        [Test]
+        public void TestSqrt() {
+            Func<double> f = () => Math.Sqrt(4.0);
+            this.Test(f);
+        }
+
+        [Test]
+        public void TestTan() {
+            Func<double> f = () => Math.Tan(1.0);
+            this.Test(f);
+        }
+
+    }
+
+}
