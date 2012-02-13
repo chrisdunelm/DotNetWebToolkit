c4a641b6a876ec4df8736b6709665684bbba4ab4
Chris Bacon <chris@dunelm.org.uk>
2012-02-13 16:50:16 +0000

    Added ToString() to Type

--- Cil2Js/JsResolvers/Classes/_Type.cs
+++ Cil2Js/JsResolvers/Classes/_Type.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using DotNetWebToolkit.Cil2Js.Ast;
+using DotNetWebToolkit.Cil2Js.Output;
 
 namespace DotNetWebToolkit.Cil2Js.JsResolvers.Classes {
 
@@ -22,6 +23,14 @@ namespace DotNetWebToolkit.Cil2Js.JsResolvers.Classes {
         [Js]
         public static Expr GetTypeFromHandle(ICall call) {
             return call.Args.First();
+        }
+
+        [Js("ToString", typeof(string))]
+        public static Stmt ToString(Ctx ctx) {
+            var eNamespace = new ExprJsTypeData(ctx, TypeData.Namespace).Named("namespace");
+            var eName = new ExprJsTypeData(ctx, TypeData.Name).Named("name");
+            var stmt = new StmtJsExplicit(ctx, "return this.namespace+\".\"+this.name;", ctx.ThisNamed, eNamespace, eName);
+            return stmt;
         }
 
     }
