3b15c149a1a903587d8ff45564fc3762c5d08214
Chris Bacon <chris@dunelm.org.uk>
2012-02-21 21:58:20 +0000

    More tests

---
+++ Test/ExecutionTests/TestEnums.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+
+namespace Test.ExecutionTests {
+
+    [TestFixture]
+    public class TestEnums : ExecutionTestBase {
+
+        enum A { a = 1, b = 2 };
+        enum B { c = 3, d = 4 };
+        A ForceA(int i) { return (A)i; }
+        B ForceB(int i) { return (B)i; }
+        B ForceB(A a) { return (B)a; }
+        public void TestCast() {
+            Func<int, int> f = a => {
+                var aa = ForceA(a);
+                var bb = ForceB(a);
+                var c = ForceB(aa);
+                return (int)aa + (int)bb + (int)c;
+            };
+            this.Test(f);
+        }
+
+    }
+
+}
---
+++ Test/ExecutionTests/TestInterfaces.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+
+namespace Test.ExecutionTests {
+
+    [TestFixture]
+    public class TestInterfaces : ExecutionTestBase {
+
+        interface I1 { int Get(int i); }
+        interface I2 { int Get(int i); }
+        interface I3 { int Get(int i); }
+        class C : I1, I2, I3 {
+            public int Get(int i) { return i * 2; }
+        }
+
+        [Test]
+        public void TestOneMethodImplementsMultipleInterfaces() {
+            Func<int, int> f = a => {
+                var c = new C();
+                var i1 = (I1)c;
+                var i2 = (I2)c;
+                var i3 = (I3)c;
+                return i1.Get(a) + i2.Get(a) + i3.Get(a);
+            };
+            this.Test(f);
+        }
+
+    }
+
+}
