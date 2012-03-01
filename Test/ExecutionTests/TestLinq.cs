6774b143c8217426c554446628452c2694bb766f
Chris Bacon <chris@dunelm.org.uk>
2012-03-01 12:08:47 +0000

    Added more LINQ tests

--- Test/ExecutionTests/TestLinq.cs
+++ Test/ExecutionTests/TestLinq.cs
@@ -10,15 +10,38 @@ namespace Test.ExecutionTests {
     [TestFixture]
     public class TestLinq : ExecutionTestBase {
 
+        #region Count
+
         [Test]
-        public void TestWhere() {
-            Func<int, int, int, int> f = (a, b, c) => {
-                var array = new[] { a, b };
-                var q = array.Where(x => x > c);
-                return q.Count();
+        public void TestCountCollection() {
+            Func<int, int> f = a => {
+                var array = new int[a];
+                return array.Count();
             };
             this.Test(f);
         }
+
+        [Test]
+        public void TestCountNonCollection() {
+            Func<int, int> f = a => {
+                var array = new int[a];
+                return array.Where(x => x >= 0).Count();
+            };
+            this.Test(f);
+        }
+
+        [Test]
+        public void TestCountPredicate() {
+            Func<int, int> f = a => {
+                var array = new int[a];
+                return array.Count(x => x >= 50);
+            };
+            this.Test(f);
+        }
+
+        #endregion
+
+        #region Select
 
         [Test]
         public void TestSelect() {
@@ -30,14 +53,17 @@ namespace Test.ExecutionTests {
         }
 
         [Test]
-        public void TestToArray() {
-            Func<int, int, int> f = (a, b) => {
-                var e = (IEnumerable<int>)new List<int> { a, b };
-                var array = e.ToArray();
-                return array[0] + array[1] * 100;
+        public void TestSelectIndex() {
+            Func<int, int, int, int, int> f = (a, b, c, d) => {
+                var array = new[] { a, b, c, d };
+                return array.Select((x, i) => x * i).Sum();
             };
             this.Test(f);
         }
+
+        #endregion
+
+        #region Sum
 
         [Test]
         public void TestSumInt32() {
@@ -56,6 +82,56 @@ namespace Test.ExecutionTests {
             };
             this.Test(f);
         }
+
+        #endregion
+
+        #region Where
+
+        [Test]
+        public void TestWhere() {
+            Func<int, int, int, int> f = (a, b, c) => {
+                var array = new[] { a, b };
+                var q = array.Where(x => x > c);
+                return q.Count();
+            };
+            this.Test(f);
+        }
+
+        [Test]
+        public void TestWhereIndex() {
+            Func<int, int, int, int, int> f = (a, b, c, d) => {
+                var array = new[] { a, b, c, d };
+                var q = array.Where((x, i) => x > 50 && i >= 2);
+                return q.Sum();
+            };
+            this.Test(f);
+        }
+
+        #endregion
+
+        #region Converters
+
+        [Test]
+        public void TestToArray() {
+            Func<int, int, int> f = (a, b) => {
+                var e = (IEnumerable<int>)new List<int> { a, b };
+                var array = e.ToArray();
+                return array[0] + array[1] * 100;
+            };
+            this.Test(f);
+        }
+
+        [Test]
+        public void TestToList() {
+            Func<int, int, int> f = (a, b) => {
+                var e = (IEnumerable<int>)new List<int> { a, b };
+                var list = e.ToList();
+                return list[0] + list[1] * 100;
+            };
+            this.Test(f);
+        }
+
+        #endregion
 
     }
 
