5bd6b22e88eecf1212729b874d8a1aaa7653f442
Chris Bacon <chris@dunelm.org.uk>
2012-03-15 22:46:13 +0000

    Added Single class

---
+++ Cil2Js/JsResolvers/Classes/_Single.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using DotNetWebToolkit.Cil2Js.Ast;
+using DotNetWebToolkit.Cil2Js.Output;
+
+namespace DotNetWebToolkit.Cil2Js.JsResolvers.Classes {
+    class _Single {
+
+        [Js]
+        public static Stmt GetHashCode(Ctx ctx) {
+            var toString = new ExprJsResolvedMethod(ctx, ctx.String, ctx.This, "toExponential");
+            var getHashCode = new ExprCall(ctx, typeof(string).GetMethod("GetHashCode"), toString);
+            var stmt = new StmtReturn(ctx, getHashCode);
+            return stmt;
+        }
+
+        [Js]
+        public static Expr IsNaN(ICall call) {
+            var ctx = call.Ctx;
+            return new ExprJsResolvedMethod(ctx, ctx.Boolean, null, "isNaN", call.Args);
+        }
+    
+    }
+}
