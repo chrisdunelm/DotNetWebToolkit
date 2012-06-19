d8673c96c5cce87f9c952ae80aca527bf218531a
Chris Bacon <chris@dunelm.org.uk>
2012-06-19 21:41:31 +0100

    Test random without explicit seed

--- Test/ExecutionTests/TestRandom.cs
+++ Test/ExecutionTests/TestRandom.cs
@@ -13,12 +13,30 @@ namespace Test.ExecutionTests {
     public class TestRandom : ExecutionTestBase {
 
         [Test]
-        public void TestR() {
-            Func<int,int> f = a => {
+        public void TestRandomInt32WithSeed() {
+            Func<int, int> f = a => {
                 var rnd = new Random(0);
                 return rnd.Next(0, Math.Abs(a & 0xf));
             };
             this.Test(f);
+        }
+
+        [Test]
+        public void TestRandomInt32NoSeed() {
+            Func<bool> f = () => {
+                const int sampleSize = 10000;
+                const int numBuckets = 10;
+                var buckets = new int[numBuckets];
+                var rnd = new Random();
+                for (int i = 0; i < sampleSize; i++) {
+                    int r = rnd.Next(0, numBuckets);
+                    buckets[r]++;
+                }
+                const int low = sampleSize / numBuckets - (sampleSize / 100);
+                const int high = sampleSize / numBuckets + (sampleSize / 100);
+                return buckets.All(x => x >= low && x <= high);
+            };
+            this.Test(f, true);
         }
 
     }
