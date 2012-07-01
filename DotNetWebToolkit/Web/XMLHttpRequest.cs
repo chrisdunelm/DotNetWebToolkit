9a14eff22117a04bcd2676f83a2d825fb0332e08
Chris Bacon <chris@dunelm.org.uk>
2012-07-01 15:54:30 +0100

    First draft of XmlHttpRequest opject, plus one simple test

---
+++ DotNetWebToolkit/Web/XMLHttpRequest.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using DotNetWebToolkit.Attributes;
+
+#pragma warning disable 0626, 0824
+
+namespace DotNetWebToolkit.Web {
+
+    [JsClass("XMLHttpRequest")]
+    public class XMLHttpRequest {
+
+        public extern XMLHttpRequest();
+
+        public extern void Abort();
+        public extern string GetAllResponseHeaders();
+        public extern string GetResponseHeader(string header);
+        public extern void Open(string method, string url);
+        public extern void Open(string method, string url, bool async);
+        public extern void Open(string method, string url, bool async, string user);
+        public extern void Open(string method, string url, bool async, string user, string password);
+        public extern void OverrideMimeType(string mimeType);
+        public extern void Send();
+        public extern void Send(ArrayBuffer data);
+        //public extern void Send(Blob data);
+        public extern void Send(Document data);
+        public extern void Send(string data);
+        //public extern void Send(FormData data);
+        public extern void SetRequestHeader(string header, string value);
+
+        [JsDetail(IsDomEvent = true)]
+        public extern Action OnReadyStateChange { set; }
+        public extern XMLHttpRequestReadyState ReadyState { get; }
+        public extern object Response { get; }
+        public extern string ResponseText { get; }
+        public extern XMLHttpRequestResponseType ResponseType { get; set; }
+        public extern Document ResponseXML { get; }
+        public extern ushort Status { get; }
+        public extern string StatusText { get; }
+        public extern uint Timeout { get; set; }
+        //public extern XMLHttpRequestUpload Upload { set; }
+        public extern bool WithCredentials { get; set; }
+
+    }
+
+    public enum XMLHttpRequestReadyState {
+        Unsent = 0,
+        Opened = 1,
+        HeadersReceived = 2,
+        Loading = 3,
+        Done = 4,
+    }
+
+    [JsStringEnum]
+    public enum XMLHttpRequestResponseType {
+        [JsDetail(Name = "")]
+        Default,
+        [JsDetail(Name = "arraybuffer")]
+        ArrayBuffer,
+        [JsDetail(Name = "blob")]
+        Blob,
+        [JsDetail(Name = "document")]
+        Document,
+        [JsDetail(Name = "json")]
+        Json,
+        [JsDetail(Name = "text")]
+        Text,
+    }
+
+    public static class XMLHttpRequestExtensions {
+
+        public static void SetTimeout(this XMLHttpRequest xmlHttpRequest, TimeSpan timeout) {
+            xmlHttpRequest.Timeout = (uint)timeout.TotalMilliseconds;
+        }
+
+        public static TimeSpan GetTimeout(this XMLHttpRequest xmlHttpRequest) {
+            return TimeSpan.FromMilliseconds(xmlHttpRequest.Timeout);
+        }
+
+    }
+
+}
---
+++ Test/BrowserTests/TestXmlHttpRequest.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using DotNetWebToolkit.Web;
+using NUnit.Framework;
+
+namespace Test.BrowserTests {
+
+    [TestFixture]
+    public class TestXmlHttpRequest : BrowserTestBase {
+
+        [Test]
+        public void TestSendRespString() {
+            this.SetUrl("/xhr", () => "abc");
+            Action f = () => {
+                var xhr = new XMLHttpRequest();
+                xhr.Open("GET", "xhr");
+                xhr.OnReadyStateChange = () => {
+                    if (xhr.ReadyState == XMLHttpRequestReadyState.Done) {
+                        Done(xhr.ResponseText == "abc");
+                    }
+                };
+                xhr.Send();
+            };
+            this.Start(f);
+        }
+
+    }
+
+}
